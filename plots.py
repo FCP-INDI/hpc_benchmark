fd6f4d28411a2564d1dbca15f1df5caa43eb5cdb
amygutierrez <amygutierrezbme@gmail.com>
2022-08-19 15:17:20 -0400

    plot CPU and memory through time of run

---
+++ plots.py
@@ -0,0 +1,32 @@
+import sys
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+one_file = str(sys.argv[1])
+
+data = pd.read_csv (one_file, header = 0)
+df = pd.DataFrame(data)
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig.add_trace(
+    go.Scatter(x=df['LOG TIME'], y=df['LOG CPU'], name="LOG CPU"),
+    secondary_y=False,
+)
+
+fig.add_trace(
+    go.Scatter(x=df['LOG TIME'], y=df['LOG MEMORY'], name="LOG MEMORY"),
+    secondary_y=True,
+)
+
+fig.update_layout(
+    title_text="LOG CPU & MEMORY"
+)
+
+fig.update_xaxes(title_text="TIME")
+
+fig.update_yaxes(title_text="LOG CPU", secondary_y=False)
+fig.update_yaxes(title_text="LOG MEMORY", secondary_y=True)
+
+fig.show()
