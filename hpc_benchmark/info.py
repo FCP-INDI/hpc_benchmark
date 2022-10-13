cfdb3f99f2939c30ab114d7550079dc06c200172
amygutierrez <amygutierrezbme@gmail.com>
2022-10-13 15:55:42 -0400

    version

--- hpc_benchmark/info.py
+++ hpc_benchmark/info.py
@@ -4,7 +4,7 @@ settings in setup.py"""
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 0
-_version_micro = 1
+_version_micro = 2
 _version_extra = ''
 
 # Construct full version string from these.
